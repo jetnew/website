8edc15da52b79cc2d683ed36fdebd40575fd24c6
Jet New <notesjet@gmail.com>
2022-12-02 03:44:57 +0800

    Revert "Updates"
    This reverts commit ebf9229bdfd8253fdd4dcb68b7b4615996885a7f.

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Jet New`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
